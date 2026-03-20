245598382afe8fe669296ce69753bde605895606
Ariadna <ariadnaperez.dev@gmail.com>
2026-03-20 13:28:48 +0100

    user cambio

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,34 @@
 const db = require("../config/db");
 
-const obtenerUsuarios = (callback) => {
-  const sql = "SELECT * FROM usuario";
+// Buscar usuario por email
+const obtenerUsuarioPorEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM usuario WHERE email = ?";
 
-  db.query(sql, (err, results) => {
-    if (err) {
-      return callback(err, null);
-    }
-    callback(null, results);
+    db.query(sql, [email], (err, results) => {
+      if (err) return reject(err);
+      resolve(results[0]); // solo uno
+    });
   });
 };
 
-module.exports = { obtenerUsuarios };
+// Crear usuario
+const crearUsuario = (email, password_hash) => {
+  return new Promise((resolve, reject) => {
+    const sql = "INSERT INTO usuario (email, password_hash) VALUES (?, ?)";
+
+    db.query(sql, [email, password_hash], (err, result) => {
+      if (err) return reject(err);
+
+      resolve({
+        id: result.insertId,
+        email,
+      });
+    });
+  });
+};
+
+module.exports = {
+  obtenerUsuarioPorEmail,
+  crearUsuario,
+};
